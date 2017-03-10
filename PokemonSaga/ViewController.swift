b7afaa3f47575dabc8bc4ebae09ba49cc74e6aa6
KR DEVARAJAN NAIR <KrishPinky@KRs-MacBook-Pro.local>
2017-03-10 05:08:28 +0200

    first commit

---
+++ PokemonSaga/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonSaga
+//
+//  Created by KR DEVARAJAN NAIR on 2017/03/10.
+//  Copyright © 2017 KR DEVARAJAN NAIR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
